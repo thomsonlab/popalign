c884c14b62d3fe2d3e7f884ebe7d7efce1757478
Paul Rivaud <paulrivaud.info@gmail.com>
2019-06-07 15:41:54 -0700

    setup

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from distutils.core import setup
 setup(
     name="popalign",
     version="0.1",
+    author="Paul Rivaud",
+    author_email='paulrivaud.info@gmail.com",
+    keywords="popalign",
     packages=[
         "popalign",
     ],
