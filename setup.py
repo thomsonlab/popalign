0a6cfa5652f5293c8c5a8723847d337a21ae86cb
Paul Rivaud <paulrivaud.info@gmail.com>
2019-06-07 11:48:25 -0700

    setup and init files

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name="popalign",
+    version="0.1",
+    packages=[
+        "methods",
+    ],
+    install_requires=[
+        "numpy",
+        "pandas",
+	"scipy",
+	"sklearn",
+	"plotly",
+	"matplotlib",
+	"seaborn",
+	"adjustText",
+	"ipywidgets"	
+    ]
+)
